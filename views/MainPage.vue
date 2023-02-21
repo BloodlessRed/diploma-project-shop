94b85236a859f627eb4183d31355f632c6eaa7b9
BloodlessRed <58694725+BloodlessRed@users.noreply.github.com>
2023-02-21 17:17:15 +0400

    Add files via upload

---
+++ views/MainPage.vue
@@ -0,0 +1,101 @@
+<template>
+    <Header></Header>
+    <div class="main-wrapper">
+        <div class="introduction">
+            <p>{{ introduction }}</p>
+        </div>
+        <div class="category-block">
+            <div class="category" >
+                <router-link v-for="category in categories" :to="{name: 'Category', params:{category:category.name}}">
+                    <span>
+                        {{ category.name }}
+                    </span>
+                    <img :src="`./img/${category.src}`">
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts" scoped>
+import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+
+export default defineComponent({
+    data() {
+        return {
+            introduction: 'Добро пожаловать в магазин компании. Здес мы продаем инструменты и запчасти от различных поставщиков для сборочного процесса',
+            categories: [
+                {
+                    name: "Nutrunners",
+                    src: "category1.svg"
+                },
+                {
+                    name: "SecondaryMarket",
+                    src: "category1.svg"
+                },
+                {
+                    name: "category3",
+                    src: "category1.svg"
+                }
+            ]
+        }
+    }
+})
+</script>
+<style scoped>
+.main-wrapper {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px 220px
+}
+
+.main-wrapper * {
+    width: 100%;
+}
+
+img {
+    max-width: fit-content;
+}
+
+.introduction {
+    display: flex;
+}
+
+.introduction:first-child {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 28px;
+}
+
+.slider {
+    flex-grow: 1;
+}
+
+.category-block {
+    display: flex;
+    justify-content: space-evenly;
+}
+
+.category {
+    display: flex;
+    justify-content: flex-start;
+
+}
+
+.category p {
+    max-width: calc(10% + 5px);
+}
+.category a {
+    width: fit-content;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+span{
+    margin:0 10px;
+}
+</style>
---
+++ views/ProductsInCategory.vue
@@ -0,0 +1,167 @@
+<template>
+    <div class="main-wrapper">
+        <div class="products">
+            <div class="item" v-for="product in category">
+                <img :src="`./img/${product.img}`" />
+                <p>{{ product.name }}</p>
+                <router-link :to="{name: 'Product', params: {category:categoryName, product:product.name}}">
+                    <div class="to-product-box">
+                        <p> Read more </p>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+        <div class="search-box">
+            <img :src="`./img/manufacturer-line.svg`" />
+            <div class="filter">
+                <h3>Производитель</h3>
+                <ul>
+                    <li>Атлас</li>
+                    <li>GSE</li>
+                    <li>Ingersoll</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    computed: {
+        category(): { img: string; name: string; prodLink: string }[] {
+            console.log(this.$route.params.category)
+            let routeParameter: string = this.$route.params.category.toString();
+            if (routeParameter == "Nutrunners") {
+                return [
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    }
+                ]
+            } else if (routeParameter == "SecondaryMarket") {
+                return [
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Second market Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Second market Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    },
+                    {
+                        img: "cable-pic-proto.svg",
+                        name: "Second market Atlas Copco 4220-0982-15 15 meter Cable",
+                        prodLink: "STUB"
+                    }
+                ]
+            }
+            return [{ img: "null", name: "null", prodLink: "null" }];
+        },
+        categoryName(): string {
+            console.log(this.$route.params);
+            return this.$route.params.category.toString();
+        }
+    },
+    data() {
+        return {
+
+        }
+    }
+
+})
+</script>
+<style scoped>
+.main-wrapper {
+    height: 100%;
+    display: flex;
+    /* justify-content: stretch; */
+    padding: 50px 220px
+}
+
+.products {
+    display: flex;
+    align-content: flex-start;
+    flex-wrap: wrap;
+    flex: 1;
+}
+
+
+.item img {
+    height: auto;
+    width: auto;
+}
+
+.item p {
+    font-family: 'Noto Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    /* line-height: 19px; */
+
+    text-align: center;
+
+    color: #000000;
+
+}
+
+.item div:last-child {
+    background: #E5E5E5;
+    border-radius: 4px;
+}
+
+.search-box {
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 24px;
+    isolation: isolate;
+
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 4px rgba(0, 0, 0, 0.25);
+}
+
+.filter {
+    padding: 0px 0px 3px 10px;
+}
+
+</style>
