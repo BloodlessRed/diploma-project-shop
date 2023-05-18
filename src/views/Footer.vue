07ede9abf33801f51ab9b0c8dddcc1c79b1a7f3c
BloodlessRed <mms28042001@gmail.com>
2023-05-18 23:34:34 +0400

    Footer fixes

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,30 +1,28 @@
 <template>
   <div class="footer-wrapper">
-    <div class="footer-main-content">
+    <div class="footer-content">
       <div class="logo">
         <img :src="`/img/logo.svg`" />
+        <div>Newtonm.com</div>
       </div>
-      <div class="site-content">
+      <!-- removed the site-sections div -->
+      <div class="site-section">
+        <!-- moved this div to the same level as the logo -->
         <p>Поставщики</p>
-        <router-link :to="{name:'Manufacturer',params:{manufacturer: supplier}}"  v-for="supplier in suppliers">
+        <router-link
+          :to="{ name: 'Manufacturer', params: { manufacturer: supplier } }"
+          v-for="supplier in suppliers"
+        >
           {{ supplier }}
-        </router-link >
-      </div>
-      <div class="site-content">
-        <p>Категории товаров</p>
-        <router-link :to="{name: 'Manufacturer', params:{manufacturer: category}}" v-for="category in categories">
-          {{ category }}
         </router-link>
       </div>
-      <div class="site-content">
-        <p>Сервисные центры</p>
-        <router-link :to="{name:'Manufacturer', params:{manufacturer: center}}" v-for="center in serviceCenters">
-          {{ center }}
-        </router-link>
+      <div class="site-section">
+        <!-- moved this div to the same level as the logo -->
+        <a>Связаться с нами</a>
       </div>
     </div>
-    <div class="copyright-wrapper">
-      <div class="copyright">©2010-2022 ”Ньютон М”</div>
+    <div class="footer-info">
+      <div class="copy">©2010-2022 ”Ньютон М”</div>
       <div class="address">
         <p>Тольятти, ул. Дзержинского 12, оф. 10</p>
         <p>ИНН 6321459534, КПП 632101001</p>
@@ -41,8 +39,8 @@ export default defineComponent({
     return {
       imgSrc: "logo.svg",
       suppliers: ["Поставщик 1", "Поставщик 2"],
-      categories: ["Категория 1", "Категория 2"],
-      serviceCenters: ["Сервисный центр 1", "Сервисный центр 2"],
+      // categories: ["Категория 1", "Категория 2"], // removed this line
+      // serviceCenters: ["Сервисный центр 1", "Сервисный центр 2"], // removed this line
     };
   },
   methods: {
@@ -54,28 +52,42 @@ export default defineComponent({
 </script>
 <style scoped>
 .footer-wrapper {
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-rows: auto auto;
   background: #161616;
 }
 
-.footer-main-content {
-  display: flex;
-  justify-content: space-evenly;
-  padding: 10px 12%;
+.footer-content {
+  display: grid;
+  grid-template-columns: auto auto auto; /* changed this from two columns to three columns to accommodate the site sections */
+  justify-content: space-around;
+  padding: 0 12%;
+  align-items: flex-start; /* added this line to align the footer content with the footer info */
 }
 
 .logo {
+  display: flex;
+  height: 100%;
+  align-items: center;
   max-width: fit-content;
 }
+.logo > div:last-of-type {
+  color: white;
+  font-weight: bold;
+  font-size: 24px;
+  margin-left: 15px;
+}
+/* removed the site-sections style */
 
-.site-content {
+.site-section {
   display: flex;
   flex-direction: column;
-  align-items: center;
+  justify-content: center;
+  height: 100%;
+  margin-bottom: 10px;
 }
 
-.site-content > p,
+.site-section > p,
 a {
   font-style: normal;
   font-weight: 400;
@@ -84,20 +96,19 @@ a {
   text-align: center;
   color: #ffffff;
 }
-.copyright-wrapper {
-  display: flex;
+.footer-info {
+  display: grid;
+  grid-template-columns: auto auto;
   justify-content: space-around;
+  align-items: flex-start;
 }
-.copyright {
+.copy {
   display: flex;
   align-items: center;
-  flex-grow: 3;
-  justify-content: flex-end;
+  height: 100%;
   color: #fff;
-  margin: 0 550px;
 }
-.address{
-    flex-grow: 3;
-    color: white;
+.address {
+  color: white;
 }
 </style>
